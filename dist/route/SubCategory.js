23e965266e2edb71769ee36a381d64a9e3e9eba1
Ariful Islam <arifulislamzyx@gmail.com>
2024-11-22 20:40:52 +0600

    dua_server

---
+++ dist/route/SubCategory.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import sqlite3 from "sqlite3";
+const router = Router();
+// Initialize the SQLite database connection
+const db = new sqlite3.Database("./src/db/dua_main.sqlite", (err) => {
+    if (err) {
+        console.error("Error connecting to the database:", err.message);
+    }
+});
+router.get("/", (req, res) => {
+    db.all("SELECT * FROM sub_category", [], (err, rows) => {
+        if (err) {
+            console.error("Error fetching categories:", err.message);
+            res.status(500).json({ error: "Failed to fetch categories" });
+            return;
+        }
+        res.json(rows);
+    });
+});
+export default router;
---
+++ src/route/Category.ts
@@ -0,0 +1,24 @@
+import { Request, Response, Router } from "express";
+import sqlite3 from "sqlite3";
+
+const router = Router();
+
+// Initialize the SQLite database connection
+const db = new sqlite3.Database("./src/db/dua_main.sqlite", (err: any) => {
+  if (err) {
+    console.error("Error connecting to the database:", err.message);
+  }
+});
+
+router.get("/", (req: Request, res: Response) => {
+  db.all("SELECT * FROM Category", [], (err: any, rows: any[]) => {
+    if (err) {
+      console.error("Error fetching categories:", err.message);
+      res.status(500).json({ error: "Failed to fetch categories" });
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+export default router;
