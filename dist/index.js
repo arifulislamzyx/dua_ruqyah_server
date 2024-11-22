8c889efbf7e5739be1318022b26482a538bb85d3
Ariful Islam <arifulislamzyx@gmail.com>
2024-11-22 22:32:45 +0600

    dua_server

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,6 @@ import dotenv from "dotenv";
 import CategoryRoute from "./route/Category";
 import DuasRoute from "./route/Dua";
 import SubCategoryRoute from "./route/SubCategory";
-import sqlite3 from "sqlite3";
-// Load environment variables
 dotenv.config();
 const app = express();
 app.use(cors());
@@ -13,23 +11,10 @@ app.use(express.json());
 app.use("/categories", CategoryRoute);
 app.use("/duas", DuasRoute);
 app.use("/subcategories", SubCategoryRoute);
-console.log("Database URL:", process.env.DATABASE_URL);
-const db = new sqlite3.Database("./src/db/dua_main.sqlite", (err) => {
-    if (err) {
-        console.log("Error Connecting Db", err.message);
-    }
-    else {
-        console.log("Db Connected");
-    }
-});
-console.log("db is here", db);
-// Define PORT with type annotation
 const PORT = parseInt(process.env.PORT || "8000", 10);
-// Example route
 app.get("/", (req, res) => {
     res.send("Server is running!");
 });
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,7 @@ import dotenv from "dotenv";
 import CategoryRoute from "./route/Category";
 import DuasRoute from "./route/Dua";
 import SubCategoryRoute from "./route/SubCategory";
-import sqlite3 from "sqlite3";
 
-// Load environment variables
 dotenv.config();
 
 const app = express();
@@ -17,27 +15,12 @@ app.use("/categories", CategoryRoute);
 app.use("/duas", DuasRoute);
 app.use("/subcategories", SubCategoryRoute);
 
-console.log("Database URL:", process.env.DATABASE_URL);
-
-const db = new sqlite3.Database("./src/db/dua_main.sqlite", (err: any) => {
-  if (err) {
-    console.log("Error Connecting Db", err.message);
-  } else {
-    console.log("Db Connected");
-  }
-});
-
-console.log("db is here", db);
-
-// Define PORT with type annotation
 const PORT: number = parseInt(process.env.PORT || "8000", 10);
 
-// Example route
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is running!");
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
